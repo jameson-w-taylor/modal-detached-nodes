0a3785753debdd8eb04079969d97055bef11d783
Jameson Taylor <jameson@ionic.io>
2024-10-24 12:29:06 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'modal-leak',
+  webDir: 'dist'
+};
+
+export default config;
